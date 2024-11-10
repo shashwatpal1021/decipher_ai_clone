97fd2591b1210d94fcde8d404707130fd204c1df
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-11-10 12:59:54 +0530

    did some changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div className='flex flex-col justify-center items-center h-[140vh] text-5xl mt-5'>
-      <div className='flex justify-center items-center mt-5'> <div className='text-5xl font-bold mr-2'>Backed by </div><img src="	https://framerusercontent.com/images/AG6R5X99jJQtHOhDNBqO7Gczl1w.svg?scale-down-to=512" alt="y-combinaton" width="50%" /></div>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex justify-center items-center mt-[17vh]'> <div className='text-[2.2vw]  mr-2'>Backed by </div><img src="	https://framerusercontent.com/images/AG6R5X99jJQtHOhDNBqO7Gczl1w.svg?scale-down-to=512" alt="y-combinaton" width="50%" /></div>
 
-      <div className="text-center text-9xl font-bold text-blue-900 mt-10">
-        The AI for<br />
-        Understanding<br />
-        Customer Behavior<br />
-        and Prioritizing<br /> Issue
+      <div className="text-center text-[6.2vw] w-[93%] font-semibold mt-[2vh] text-blue-900 tracking-tight leading-[1.05]">
+        The AI for Understanding Customer Behavior and Prioritizing Issue
       </div>
 
       <div>Our AI agent reviews session replays to interpret <br />
-      customer and product behavior, helping your team<br/> prioritize issues, imporve the product, </div>
+        customer and product behavior, helping your team<br /> prioritize issues, imporve the product, </div>
     </div>
   )
 }
